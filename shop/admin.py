4e06968b2a9b467003865fad161ec37b790d1787
Kaluga-Fence <kaluga-fence@yandex.ru>
2025-08-12 09:12:07 +0300

    Create admin.py

---
+++ shop/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Product, Order
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'size', 'price', 'available')
+    list_filter = ('category', 'available')
+    list_editable = ('available',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'address', 'created_at', 'status')
+    list_filter = ('status', 'created_at')
